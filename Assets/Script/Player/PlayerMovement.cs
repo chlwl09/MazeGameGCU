2e73f93a51c101ac56544d9b5e97c7b210d0497e
Choi tho <69807772+chlwl09@users.noreply.github.com>
2024-07-09 18:57:50 +0900

    미로 완성

---
+++ Assets/Script/Player/PlayerMovement.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed = 10.0f;
+
+    public float rotateSpeed = 10.0f;
+
+    public float jumpForce = 10.0f;
+
+    private bool isGround = true;
+
+    Rigidbody body;                      
+
+    float h, v;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();  
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(h, 0, v);
+
+        if (!(h == 0 && v == 0))
+        {
+            transform.position += dir * Speed * Time.deltaTime;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * rotateSpeed);
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space) && isGround)
+        {
+
+            body.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+            isGround = false;
+        }
+    }
+
+    // 충돌 함수
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;
+        }
+    }
+}
